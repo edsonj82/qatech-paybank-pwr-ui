d7726bd2490ce492461d1a954b9aeae451eceb79
Edson <edsonj82@outlook.com>
2025-09-20 16:34:44 -0300

    developing login scenarios (wip) - login successfully (applying pgPromise)

---
+++ support/db.js
@@ -0,0 +1,18 @@
+import pgPromise from 'pg-promise';
+
+const pgp = pgPromise();
+const db = pgp('postgresql://dba:dba@paybank-db:5432/UserDB');
+
+export async function getCode2FA() {
+    const query = `
+    SELECT code
+	FROM public."TwoFactorCode"
+	ORDER BY id DESC
+	LIMIT 1;
+    `
+    const result = await db.oneOrNone(query)
+    return result.code
+
+}
+
+
